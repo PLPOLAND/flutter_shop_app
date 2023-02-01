b1295900d1923daec6fe1274f9e55a3e07a51e5a
PLPOLAND <marekpaldyna@wp.pl>
2023-02-01 09:40:05 +0100

    theaming

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,20 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        colorScheme: const ColorScheme.light(
+            primary: Color.fromARGB(255, 255, 35, 35),
+            onPrimary: Colors.white,
+            secondary: Color.fromARGB(255, 243, 100, 33),
+            onSecondary: Colors.black,
+            background: Color.fromARGB(255, 255, 173, 167)),
+      ),
       home: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.primary,
         appBar: AppBar(
-          title: const Text('Hello World'),
+          title: const Text('MyShop'),
         ),
         body: const Center(
           child: Text('Hello World!'),
