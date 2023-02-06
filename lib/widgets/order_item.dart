a5848bc0970a8f3b26cbffa06fe5795c38bf9f6d
Marek Pałdyna <marekpaldyna@wp.pl>
2023-02-06 17:46:37 +0100

    end of section 8

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,14 +1,22 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import '../providers/orders.dart' as ord;
 
 import 'package:intl/intl.dart';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   const OrderItem({super.key, required this.order});
 
   final ord.OrderItem order;
 
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -18,15 +26,50 @@ class OrderItem extends StatelessWidget {
         child: Column(
           children: [
             ListTile(
-              title: Text('\$ ${order.amount}'),
+              title: Text('\$ ${widget.order.amount}'),
               subtitle: Text(
-                DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
               ),
               trailing: IconButton(
-                icon: const Icon(Icons.expand_more),
-                onPressed: () {},
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
               ),
             ),
+            if (_expanded) Divider(),
+            if (_expanded)
+              Container(
+                height: min(widget.order.products.length * 20.0 + 10, 100),
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                child: ListView(
+                  children: widget.order.products
+                      .map(
+                        (prod) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              prod.title,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '${prod.quantity}x \$${prod.price}',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
           ],
         ),
       ),
